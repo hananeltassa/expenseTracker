0b524369c14f3c49b7b74f766bf7e54fbeff4bb9
hananeltassa <hananeltassa15@gmail.com>
2024-11-07 23:32:10 +0200

    Added login.php

--- login.php
+++ login.php
@@ -2,3 +2,28 @@
 
 require_once 'connection.php';
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = $connection->prepare("SELECT id, username, email, password FROM users WHERE username = ?");
+    $query->bind_param("s", $username);
+    $query->execute();
+    $result = $query->get_result();
+
+    if ($result->num_rows == 1) {
+        $user = $result->fetch_assoc();
+
+        if (password_verify($password, $user['password'])) {
+            header("Location: index.html"); 
+            exit();
+        } else {
+            echo "Incorrect password!";
+        }
+    } else {
+        echo "Username not found!";
+    }
+
+    $query->close();
+}
+?>
